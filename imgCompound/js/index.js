cbc1e17279e145e601ba8a48db04720b4835f5c2
wuyuanaaa <dao332508553@163.com>
2018-07-27 15:58:39 +0800

    更新图片合成逻辑

--- imgCompound/js/index.js
+++ imgCompound/js/index.js
@@ -1,131 +1,78 @@
 $(function() {
-	// 接收图片
-	var mainImgBox = $('#mainImgBox');
-	var wxImgBox = $('#wxImgBox');
-	var mainFileInput = $('#mainFileInput');
-	var wxFileInput = $('#wxFileInput');
-	var downloadBtn = $('#downloadBtn');
-	var mainName = $('#mainName');
-	var wxName = $('#wxName');
+	var $mainImgBox = $('#mainImgBox');
+	var $codeImgBox = $('#codeImgBox');
+	var $mainFileInput = $('#mainFileInput');
+	var $codeFileInput = $('#wxFileInput');
+	var $downloadBtn = $('#downloadBtn');
+	var $mainName = $('#mainName');
+	var $codeName = $('#codeName');
 	var $code = $('#code');
-	var $qrcodeCanvas = $('#qrcodeCanvas');
+	var $finallyImg = $('#finallyImg');
 	var imgArr = [];
 	var wxXY = {};
 	var mainWH = {};
 	var imgBase64;
-	var automation = true;
+	var $radios = $("input[name='mode']");
+	var automation = true;  // 是否为快速模式
 
 	// 拖拽上传图片
-	mainImgBox[0].ondragenter = function (event) {
+	$mainImgBox[0].ondragenter = function (event) {   // 清除默认事件，防止浏览器直接打开图片文件
 		event.stopPropagation();
 		event.preventDefault();
 	};
-	mainImgBox[0].ondragover = function (event) {
+	$mainImgBox[0].ondragover = function (event) {
 		event.stopPropagation();
 		event.preventDefault();
 	};
-	wxImgBox[0].ondragenter = function (event) {
+	$codeImgBox[0].ondragenter = function (event) {
 		event.stopPropagation();
 		event.preventDefault();
 	};
-	wxImgBox[0].ondragover = function (event) {
+	$codeImgBox[0].ondragover = function (event) {
 		event.stopPropagation();
 		event.preventDefault();
 	};
 
-	mainImgBox[0].ondrop = function (event) {
+	$mainImgBox[0].ondrop = function (event) {
 		event.stopPropagation();
 		event.preventDefault();
 		var file = event.dataTransfer.files[0];
-		mainImgUp(file);
+		mainImgUpload(file);
 	};
-	wxImgBox[0].ondrop = function (event) {
+	$codeImgBox[0].ondrop = function (event) {
 		event.stopPropagation();
 		event.preventDefault();
 		var file = event.dataTransfer.files[0];
-		wxImgUp(file);
+		codeImgUpload(file);
 	};
 
 	// 点击上传图片
-	$('.box').on('click',function (event) {
+	$('.box').on('click',function (event) {  // 父盒子的点击事件传递给file input
 		event.stopPropagation();
 		$(this).find('.file')[0].click();
 	});
-	mainFileInput.on('change',function () {
-		var file = mainFileInput[0].files[0];
-		mainImgUp(file);
+	$mainFileInput.on('change',function () {
+		mainImgUpload(this.files[0]);
 	});
-	wxFileInput.on('change',function () {
-		var file = wxFileInput[0].files[0];
-		wxImgUp(file);
+	$codeFileInput.on('change',function () {
+		codeImgUpload(this.files[0]);
 	});
-
-
-
-	// 二维码坐标设置
-	var $radios2 = $("input[name='xy']");
-
-    $radios2.on('click',function () {
-		if($(this).hasClass('on')){
-            layer.msg('当前切换到【快速模式】，将智能识别二维码进行拼图！');
-        }else {
-            automation = false;
-            layer.msg('当前切换到【自由模式】，按住鼠标拖放即可移动二维码！');
-
-			$qrcodeCanvas.on('mousedown', function (event) {
-				event.preventDefault();
-				$code.show().css({
-					top: event.offsetY + 'px',
-					left: event.offsetX + 'px'
-				});
-				wxXY.x = event.offsetX;
-				wxXY.y = event.offsetY;
-				window.onmousemove = function (event) {
-					var w = event.clientX  - $code.offset().left;
-					$code.width(w*2);
-					$code.height(w*2);
-					wxXY.w = w*2;
-				};
-				window.onmouseup = function () {
-					window.onmousemove = null;
-					$code.hide();
-					compound();
-				};
-			});
-
-		}
-	});
-
-	// 下载图片
-	downloadBtn.on('click',function () {
-		download();
-	});
-
-	// 返回海报图宽高
-	function getMainWH(src) {
-		var img = new Image;
-		img.src = src;
-		img.onload = function (ev) {
-			mainWH.w = img.width;
-			mainWH.h = img.height;
-		}
-	}
 
 	// 图片上传事件
-	function mainImgUp(file) {
-		mainName.text(file.name);
+	function mainImgUpload(file) {
+		wxXY = {};
+		$mainName.text(file.name);
 		layer.msg('主图上传成功！');
 		var reader = new FileReader();
 		reader.onload = function (event) {
 			imgArr[0] = event.target.result;
-			compound();
-			getMainWH(event.target.result); // 获取海报的宽高
+			getMainWH(event.target.result); // 获取海报的宽高后开始合成
 		};
 		reader.readAsDataURL(file);
 	}
-	function wxImgUp(file) {
-		wxName.text(file.name);
-        layer.msg('二维码上传成功！');
+	function codeImgUpload(file) {
+		$codeName.text(file.name);
+		layer.msg('二维码上传成功！');
 		var reader = new FileReader();
 		reader.onload = function (event) {
 			imgArr[1] = event.target.result;
@@ -134,63 +81,94 @@ $(function() {
 		reader.readAsDataURL(file);
 	}
 
+	// 二维码位置模式设置
+    $radios.on('click',function () {
+		if($(this).hasClass('on')){
+            layer.msg('已切换到【快速模式】，将智能识别二维码进行拼图！');
+        }else {
+			layer.msg('已切换到【自由模式】，按住鼠标拖放即可移动二维码！');
+			switchToMode2();
+		}
+	});
 
-	// 下载图片事件
-	function download() {
-		var imgData = imgBase64;
-		imgData = imgData.replace('image/jpeg','image/octet-stream');
-		var saveFile = function (data,filename) {
-			var link = document.createElement('a');
-			link.href = data;
-			link.download = filename;
-			var event = document.createEvent('MouseEvents');
-			event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-			link.dispatchEvent(event);
-		};
-		var filename = new Date().toLocaleDateString() + '.' + 'jpg';
-		saveFile(imgData, filename);
+	// 自由模式下二维码位置改变
+    function switchToMode2() {
+		automation = false;
+		$finallyImg.on('mousedown', function (event) {
+			event.preventDefault();
+			$code.show().css({
+				top: event.offsetY + 'px',
+				left: event.offsetX + 'px'
+			});
+			wxXY.x = event.offsetX;
+			wxXY.y = event.offsetY;
+			window.onmousemove = function (event) {
+				var w = event.clientX  - $code.offset().left;
+				$code.width(w*2);
+				$code.height(w*2);
+				wxXY.w = w*2;
+			};
+			window.onmouseup = function () {
+				window.onmousemove = null;
+				$code.hide();
+				compound();
+			};
+		});
 	}
+
+	// 返回海报图宽高
+	function getMainWH(src) {
+		var img = new Image;
+		img.src = src;
+		img.onload = function () {
+			mainWH.w = img.width;
+			mainWH.h = img.height;
+			compound(); // 启动合成
+		}
+	}
+
 	//生成画布
 	function compound(){
 		draw(function(){
-			document.getElementById('qrcodeCanvas').innerHTML='<img src="'+imgBase64+'">';
+			$finallyImg.html('<img src="'+imgBase64+'">');
 		})
 	}
 	function draw(fn) {
 		var c = document.createElement('canvas'),
 			ctx = c.getContext('2d'),
-			len = imgArr.length,
             result = {};
-
-		result.x = wxXY.x;
-		result.y = wxXY.y;
-		result.w = wxXY.w;
+		result = {
+			x: wxXY.x,
+			y: wxXY.y,
+			w: wxXY.w
+		};
 		c.width = mainWH.w;
 		c.height = mainWH.h;
 
 		function drawing(n) {
-			if (n<len) {
+			if (n < imgArr.length) {
 				var img = new Image;
 				img.src = imgArr[n];
 				img.onload = function() {
 					if (n==1) {
                         ctx.drawImage(img,result.x,result.y,result.w,result.w);
 						drawing(n+1);
-                        layer.msg('拼图成功，点击【下载海报】即可下载！');
+						console.log(result);
+						if(result.w){
+							layer.msg('拼图成功，点击【下载海报】即可下载！');
+						}else {
+							layer.msg('拼图失败，建议使用【自由模式】并手动设定二维码位置！');
+						}
 					} else {
 						ctx.drawImage(img,0,0,img.width,img.height);
 						if(automation) {
                             var data = ctx.getImageData(0, 0, img.width, img.height).data;
-                            result = getWxInfo(data,img.width,img.height);
-                            if(result) {
-                                drawing(n+1);
-                            }else {
-                                layer.msg('自动生成失败，请使用【点击模式】！');
-                            }
+                            result = getCodeInfo(data,img.width,img.height);
+							console.log(result);
+							drawing(n+1);
 						}else {
                             drawing(n+1);
 						}
-
 					}
 				}
 			} else {
@@ -202,7 +180,7 @@ $(function() {
 	}
 
     // 图片像素点处理
-    function getWxInfo(data,w,h) {
+    function getCodeInfo(data,w,h) {
         var arr = [];
         for(var i = 0; i < h; i ++){
             for(var j = 0; j < w; j++){
@@ -249,7 +227,6 @@ $(function() {
                         times = 0;
                         XArr[times] = arr[i].x
                     }
-
                 }
             }
         }
@@ -273,9 +250,27 @@ $(function() {
                 }
             }
             if(!find) {
-                // getY();  //  如果没有找到Y直线  继续找下一条X直线
+                 getY();  //  如果没有找到Y直线  继续找下一条X直线
             }
         }
     }
 
+	// 下载按钮点击事件
+	$downloadBtn.on('click',download);
+
+	// 下载图片事件
+	function download() {
+		var imgData = imgBase64;
+		imgData = imgData.replace('image/jpeg','image/octet-stream');
+		function saveFile(data,filename) {
+			var link = document.createElement('a');
+			link.href = data;
+			link.download = filename;
+			var event = document.createEvent('MouseEvents');
+			event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+			link.dispatchEvent(event);
+		}
+		var filename = new Date().toLocaleDateString() + '.' + 'jpg';
+		saveFile(imgData, filename);
+	}
 });
